1fa942e33a0dd9042d29ae61231bd6ce41ee31a2
thiagorazzini <razzini.thiago@gmail.com>
2023-03-15 18:59:40 -0300

    [Aula-1] Developer api to post and get

---
+++ Portifolio.Api/PortifolioContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Portifolio.Api
+{
+    public class PortifolioContext : DbContext
+    {
+        public PortifolioContext(DbContextOptions<PortifolioContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("ServerConnection"));
+        }
+
+    }
+}
